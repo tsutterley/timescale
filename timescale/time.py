fix: gps week calculation

timescale/time.py
@@ -823,7 +823,7 @@ class Timescale:
     def gps_week(self):
         """GPS week number since 1980-01-06T00:00:00
         """
-        return np.mod(self.tt - 2444244.5, 7).astype(np.int64)
+        return ((self.tt - 2444244.5)//7).astype(np.int64)
 
     @timescale.utilities.reify
     def J2000(self):