f589dcc6f5573e9d8d9cbd1b563dda687c3c0b44
Tyler Sutterley <tsutterl@uw.edu>
2023-09-12 11:09:20 -0700

    ci: set micromamba version to `latest` (#10)
    docs: use micromamba for RTD builds
    docs: add logo and adjust style

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -72,14 +72,14 @@ html_show_copyright = True
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-# html_theme_options = {
-#     "logo_only": True,
-# }
+html_theme_options = {
+    "logo_only": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_logo = "_assets/timescale_logo.png"
+html_logo = "_assets/timescale_logo.png"
 html_static_path = ['_static']
 repository_url = f"https://github.com/tsutterley/timescale"
 html_context = {
